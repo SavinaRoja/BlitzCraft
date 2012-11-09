ccd612499151552989c3984f185d77cdf4a1aafa
SavinaRoja <pablo.barton@gmail.com>
2012-11-09 12:25:34 -0500

    Migrated screenshot over to utils, have made screenshot support modular by platform

--- src/blitzcraft/utils.py
+++ src/blitzcraft/utils.py
@@ -1,4 +1,59 @@
+import sys
+import gtk.gdk
 
+### Screenshot methods for different platforms ###
+def mac_screenshot():
+    '''
+    Takes a screenshot on a mac platform.
+    
+    This method expects coordinates for the upper left corner of the game
+    window and the lower right. These should be provided as a pair either
+    as a list, [x,y] or as a tuple, (x,y). A buffer may be specified and its
+    integer value will be used to extend the screenshot's length and width.
+    '''
+    #I REALLY don't know how to do this yet
+    pass
+
+def windows_screenshot():
+    '''
+    Takes a screenshot on a windows platform.
+    
+    This method expects coordinates for the upper left corner of the game
+    window and the lower right. These should be provided as a pair either
+    as a list, [x,y] or as a tuple, (x,y). A buffer may be specified and its
+    integer value will be used to extend the screenshot's length and width.
+    '''
+    #I don't know how I want to do this yet
+    pass
+
+def unix_screenshot(upper_left=[0,0], lower_right=[1,1], buffer=1, save=False):
+    '''
+    
+    Takes a screenshot on a unix/linux platform.
+    
+    This method expects coordinates for the upper left corner of the game
+    window and the lower right. These should be provided as a pair either
+    as a list, [x,y] or as a tuple, (x,y) .A buffer may be specified and its
+    integer value will be used to extend the screenshot's length and width.
+    
+    This method returns a NumPy array of the pixels in the screenshot, be
+    aware that the array will invert the axes to (y,x) and the indices will
+    begin at 0.
+    '''
+    #Get individual coordinates
+    ul_x, ul_y = upper_left[0], upper_left[1]
+    lr_x, lr_y = lower_right[0], lower_right[1]
+    #Get the gtk.gdk window
+    w = gtk.gdk.get_default_root_window()
+    size = (lr_x - ul_x + buffer, lr_y - ul_y + buffer)
+    pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB,False,8,size[0],size[1])
+    pb = pb.get_from_drawable(w,w.get_colormap(),ul_x,ul_y,0,0,size[0],size[1])
+    #You can save the images with code like:
+    if save:
+        pb.save('test.png', 'png')
+    #Returns the NumPy array, axes are inverted (y,x)
+    return pb.get_pixels_array()
+    
 
 def define_game_window(mouse):
     '''
@@ -30,3 +85,11 @@ Bejeweled Blitz game window. Then press Enter.\n''')
         return True
     else:
         return False
+
+### For different platforms, load up different methods ###
+if sys.platform == 'darwin':  # Mac
+    screenshot = mac_screenshot
+elif sys.platform == 'win32':  # Windows
+    screenshot = windows_screenshot
+else:  # Unix/Linux
+    screenshot = unix_screenshot
