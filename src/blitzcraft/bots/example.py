7d2aa2e75157a4b535d48a467b097022d23ff560
SavinaRoja <pablo.barton@gmail.com>
2012-11-09 12:27:37 -0500

    An initial example of inheritance and overriding of the BlitzBot base class

---
+++ src/blitzcraft/bots/example.py
@@ -0,0 +1,42 @@
+'''An example of how to build your own bot based on the BlitzBot base class'''
+
+from blitzcraft.blitzbot import BlitzBot
+
+class MyBot(BlitzBot):
+    '''A bot of my own'''
+    
+    def __init__(self):
+        BlitzBot.__init__(self)
+        print('Running my own bot based on BlitzBot')
+
+    #Here I have overridden the parent class' method for reading the board to
+    #allow it to average pixel data for a piece of each gem instead of using
+    #only a single gem.
+    def read_blitz_board(self):
+        '''
+        Takes a screenshot of the game window containing the gems and extracts
+        pixel data in order to compile a fresh 8x8 array for self.board. This
+        method uses a 4x4 pixel window for each gem and uses the average RGB.
+        '''
+        #We only need a minimal screenshot which contains all the gem pixels
+        upper = self.gem_keys[0][0]
+        lower = self.gem_keys[7][7]
+        screen = self.screenshot(upper, lower, buffer=4)
+        #Iterate over our gem key positions in order to color the board
+        board = []
+        for i in self.gem_keys:  # X dimension
+            board_col = []
+            for j in i:  # Y dimension
+                gem_x, gem_y = j[0], j[1]
+                #Extract a 4x4 pixel slice
+                slice = []
+                for a in xrange(4):
+                    col = []
+                    for b in xrange(4):
+                        gem_pixel = screen[gem_y-upper[1]+a][gem_x-upper[0]+b]
+                        col.append(gem_pixel)
+                    slice.append(col)
+                board_col.append(self.determine_average_color(slice))
+            board.append(board_col)
+        self.board = board
+
