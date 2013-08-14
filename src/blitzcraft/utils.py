ba10ad51762d68282b672c7247b0e726f40c324b
Paul Barton <pablo.barton@gmail.com>
2013-08-14 11:23:30 -0400

    Added todo statements for mac and windows screenshot support

--- src/blitzcraft/utils.py
+++ src/blitzcraft/utils.py
@@ -5,37 +5,37 @@ import gtk.gdk
 def mac_screenshot():
     '''
     Takes a screenshot on a mac platform.
-    
+
     This method expects coordinates for the upper left corner of the game
     window and the lower right. These should be provided as a pair either
     as a list, [x,y] or as a tuple, (x,y). A buffer may be specified and its
     integer value will be used to extend the screenshot's length and width.
     '''
-    #I REALLY don't know how to do this yet
+    #TODO: Add in mac screenshotting
     pass
 
 def windows_screenshot():
     '''
     Takes a screenshot on a windows platform.
-    
+
     This method expects coordinates for the upper left corner of the game
     window and the lower right. These should be provided as a pair either
     as a list, [x,y] or as a tuple, (x,y). A buffer may be specified and its
     integer value will be used to extend the screenshot's length and width.
     '''
-    #I don't know how I want to do this yet
+    #TODO: Add in windows screenshotting
     pass
 
 def unix_screenshot(upper_left=[0,0], lower_right=[1,1], buffer=1, save=False):
     '''
-    
+
     Takes a screenshot on a unix/linux platform.
-    
+
     This method expects coordinates for the upper left corner of the game
     window and the lower right. These should be provided as a pair either
     as a list, [x,y] or as a tuple, (x,y) .A buffer may be specified and its
     integer value will be used to extend the screenshot's length and width.
-    
+
     This method returns a NumPy array of the pixels in the screenshot, be
     aware that the array will invert the axes to (y,x) and the indices will
     begin at 0.
@@ -46,36 +46,41 @@ def unix_screenshot(upper_left=[0,0], lower_right=[1,1], buffer=1, save=False):
     #Get the gtk.gdk window
     w = gtk.gdk.get_default_root_window()
     size = (lr_x - ul_x + buffer, lr_y - ul_y + buffer)
-    pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB,False,8,size[0],size[1])
-    pb = pb.get_from_drawable(w,w.get_colormap(),ul_x,ul_y,0,0,size[0],size[1])
+    pb = gtk.gdk.Pixbuf(gtk.gdk. COLORSPACE_RGB, False, 8, size[0], size[1])
+    pb = pb.get_from_drawable(w, w.get_colormap(),
+                              ul_x, ul_y, 0, 0, size[0], size[1])
     #You can save the images with code like:
     if save:
         pb.save('test.png', 'png')
     #Returns the NumPy array, axes are inverted (y,x)
     return pb.get_pixels_array()
-    
+
 
 def define_game_window(mouse):
     '''
     Asks for user input to define the dimension of the game window; writes
     these dimensions to the blitz_window.txt file.
     '''
-    
+
     #Get the upper left corner
-    print('''\nPlease place the mouse on the upper left hand corner of the \
-Bejeweled Blitz game window. Then press Enter.\n''')
+    print('''
+Please place the mouse on the upper left hand corner of the \
+Bejeweled Blitz game window. Then press Enter.
+''')
     raw_input('Upper left corner: ')
     mouse_x1, mouse_y1 = mouse.position()
     print('(X: {0}, Y: {1})'.format(mouse_x1, mouse_y1))
-    
+
     #Get the lower right corner
-    print('''\nNow place the mouse on the lower right hand corner of the \
-Bejeweled Blitz game window. Then press Enter.\n''')
+    print('''
+Now place the mouse on the lower right hand corner of the \
+Bejeweled Blitz game window. Then press Enter.
+''')
     raw_input('Lower right corner: ')
     mouse_x2, mouse_y2 = mouse.position()
     #Get the upper left corner
     print('(X: {0}, Y: {1})'.format(mouse_x2, mouse_y2))
-    
+
     #Ask if these should be saved
     print('Would you like to save these coordinates?')
     if raw_input('[y/N] ') in ['y', 'Y']:
